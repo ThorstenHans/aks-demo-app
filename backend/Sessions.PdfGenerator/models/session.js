3f019972d3e836eb24c218fe864308cf2338714b
Thorsten Hans <thorsten.hans@gmail.com>
2018-06-18 15:23:31 +0200

    fix(export): Remove PDF stuff for now

--- backend/Sessions.PdfGenerator/models/session.js
+++
@@ -1,9 +0,0 @@
-class Session {
-    constructor(title, abstract, speaker) {
-        this.title = title;
-        this.abstract = abstract;
-        this.speaker = speaker;
-    }
-}
-
-module.exports = Session;
