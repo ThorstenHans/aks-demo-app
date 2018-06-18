52cd1e9e17c2942d25b4131615db55b58bc68b28
Thorsten Hans <thorsten.hans@gmail.com>
2018-06-18 15:53:13 +0200

    chore(terraform): apply proper indention to tf

--- terraform/deployment.tf
+++ terraform/deployment.tf
@@ -1,71 +1,71 @@
-provider "azurerm" { }
+provider "azurerm" {}
 
 resource "azurerm_resource_group" "demo" {
-    name = "${var.resource_group_name}"
-    location = "West Europe"
-    tags = "${var.tags}"
+  name     = "${var.resource_group_name}"
+  location = "West Europe"
+  tags     = "${var.tags}"
 }
 
 resource "azurerm_resource_group" "demoaks" {
-    name = "${var.aks_resource_group_name}"
-    location = "West Europe"
-    tags = "${var.tags}"
+  name     = "${var.aks_resource_group_name}"
+  location = "West Europe"
+  tags     = "${var.tags}"
 }
 
 resource "azurerm_sql_server" "demo" {
-    name = "thh-demo-sql-server"
-    resource_group_name = "${azurerm_resource_group.demo.name}"
-    location = "${azurerm_resource_group.demo.location}"
-    version = "12.0"
-    administrator_login = "testadmin"
-    administrator_login_password = "thisIsSuperSecur3!"
-    tags = "${var.tags}"
+  name                         = "thh-demo-sql-server"
+  resource_group_name          = "${azurerm_resource_group.demo.name}"
+  location                     = "${azurerm_resource_group.demo.location}"
+  version                      = "12.0"
+  administrator_login          = "testadmin"
+  administrator_login_password = "thisIsSuperSecur3!"
+  tags                         = "${var.tags}"
 }
+
 resource "azurerm_sql_database" "demo" {
-    name = "sessions"
-    resource_group_name = "${azurerm_resource_group.demo.name}"
-    location = "${azurerm_resource_group.demo.location}"
-    server_name = "${azurerm_sql_server.demo.name}"
-    tags = "${var.tags}"
+  name                = "sessions"
+  resource_group_name = "${azurerm_resource_group.demo.name}"
+  location            = "${azurerm_resource_group.demo.location}"
+  server_name         = "${azurerm_sql_server.demo.name}"
+  tags                = "${var.tags}"
 }
 
 resource "azurerm_container_registry" "test" {
-    name                = "${var.acr_name}"
-    resource_group_name = "${azurerm_resource_group.demo.name}"
-    location            = "${azurerm_resource_group.demo.location}"
-    admin_enabled       = false
-    sku                 = "${var.acr_sku}"
-    tags = "${var.tags}"
+  name                = "${var.acr_name}"
+  resource_group_name = "${azurerm_resource_group.demo.name}"
+  location            = "${azurerm_resource_group.demo.location}"
+  admin_enabled       = false
+  sku                 = "${var.acr_sku}"
+  tags                = "${var.tags}"
 }
 
 resource "azurerm_kubernetes_cluster" "demo" {
-    name = "${var.aks_name}"
-    location = "${azurerm_resource_group.demo.location}"
-    resource_group_name = "${azurerm_resource_group.demoaks.name}"
-    kubernetes_version = "${var.k8s_version}"
-    
-    linux_profile {
-        admin_username = "azureuser"
-        
-        ssh_key {
-            key_data = "${file("${var.ssh_public_key}")}"
-        }
+  name                = "${var.aks_name}"
+  location            = "${azurerm_resource_group.demo.location}"
+  resource_group_name = "${azurerm_resource_group.demoaks.name}"
+  kubernetes_version  = "${var.k8s_version}"
+
+  linux_profile {
+    admin_username = "azureuser"
+
+    ssh_key {
+      key_data = "${file("${var.ssh_public_key}")}"
     }
+  }
 
-    dns_prefix = "thhtestprefix" 
+  dns_prefix = "thhtestprefix"
 
-    agent_pool_profile {
-        name = "default"
-        count = "${var.aks_agent_vm_count}"
-        vm_size = "${var.aks_agent_vm_size}"
-        os_type = "Linux"
-    }
+  agent_pool_profile {
+    name    = "default"
+    count   = "${var.aks_agent_vm_count}"
+    vm_size = "${var.aks_agent_vm_size}"
+    os_type = "Linux"
+  }
 
-    service_principal {
-        client_id = "${var.service_principal_id}"
-        client_secret = "${var.service_principal_password}"
-    }
-    tags = "${var.tags}"
+  service_principal {
+    client_id     = "${var.service_principal_id}"
+    client_secret = "${var.service_principal_password}"
+  }
 
+  tags = "${var.tags}"
 }
-
--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,55 +1,56 @@
 variable "service_principal_id" {
-    type = "string"
+  type = "string"
 }
 
 variable "service_principal_password" {
-    type = "string"
+  type = "string"
 }
 
 variable "ssh_public_key" {
-    type = "string"
+  type = "string"
 }
 
 variable "resource_group_name" {
-    type = "string"
+  type = "string"
 }
 
 variable "aks_resource_group_name" {
-    type = "string"
+  type = "string"
 }
 
 variable "tags" {
-    type = "map"
-    default = {
-        Environment = "Demo"
-        Responsible = "Thorsten Hans"
-    }
-} 
+  type = "map"
+
+  default = {
+    Environment = "Demo"
+    Responsible = "Thorsten Hans"
+  }
+}
 
 variable "acr_name" {
-    type = "string"
+  type = "string"
 }
 
 variable "acr_sku" {
-    type = "string"
-    default = "Basic"
+  type    = "string"
+  default = "Basic"
 }
 
 variable "aks_name" {
-    type = "string"
+  type = "string"
 }
 
 variable "aks_agent_vm_size" {
-    type = "string"
-    default = "Standard_D2_v2"
+  type    = "string"
+  default = "Standard_D2_v2"
 }
 
 variable "aks_agent_vm_count" {
-    type = "string"
-    default = "2"
+  type    = "string"
+  default = "2"
 }
 
 variable "k8s_version" {
-    type = "string"
-    default = "1.9.6"
+  type    = "string"
+  default = "1.9.6"
 }
